dab57d4d1d018065045deca3eee8e553a9dd15ec
honeymon <ihoneymon@gmail.com>
2018-02-04 12:36:10 +0900

    Init Project: Honeymon spring boot tester

---
+++ src/main/java/io/honeymon/springboot/tester/Application.java
@@ -0,0 +1,15 @@
+package io.honeymon.springboot.tester;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Spring Boot 를 테스트 하자!!
+ */
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
